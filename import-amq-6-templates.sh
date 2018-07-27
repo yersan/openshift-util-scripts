f03e7214a70a422cc1b52d93e4df4f84c79e96e6
Ken Wills <ken@zaptillion.net>
2018-07-27 14:51:03 -0500

    drop AMQ 6.2 imagestream

--- import-amq-6-templates.sh
+++ import-amq-6-templates.sh
@@ -1,5 +1,4 @@
-for resource in amq/amq62-image-stream.json \
-  amq/amq63-image-stream.json 
+for resource in amq/amq63-image-stream.json
 do
   oc replace -n openshift --force -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/$resource
 done
